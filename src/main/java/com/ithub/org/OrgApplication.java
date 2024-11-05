274e08dd5ef17cab18e0cad9207c355dff4595cd
ratmir <ratmir.yuldashev28@mail.ru>
2024-11-05 19:15:59 +0300

    add another data

---
+++ src/main/java/com/ithub/org/OrgApplication.java
@@ -0,0 +1,11 @@
+package com.ithub.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrgApplication.class, args);
+	}
+}
